8a306fbce4301b712f5399489c45bf24d881ef54
overstimulation <146885536+overstimulation@users.noreply.github.com>
2025-04-25 11:08:35 +0200

    Lab7: Zad5

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import requests
-import PySide6
-import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
 
+import matplotlib.pyplot as plt
+import PySide6
+import requests
 
 latitude = 51.25
 longitude = 22.57
@@ -24,20 +24,25 @@ data = response.json()
 
 format = "%Y-%m-%dT%H:%M"
 temperature_2m = data["hourly"]["temperature_2m"]
-apparent_temperature = data['hourly']['apparent_temperature']
+apparent_temperature = data["hourly"]["apparent_temperature"]
+precipitation = data["hourly"]["precipitation"]
 print(temperature_2m)
 x = [datetime.strptime(i, format) for i in data["hourly"]["time"]]
 print(x)
-plt.figure(figsize=(6,4))
-plt.plot(x, temperature_2m, label='Temperatura', color='red')
-plt.plot(x, apparent_temperature, label='Temperatura odczuwalna')
-plt.legend()
-plt.ylabel('temperatura')
-plt.xlabel("czas")
-plt.xticks(rotation = 45)
-plt.title('Wykres')
-plt.grid(True)
+# plt.figure(figsize=(6,4))
+fig, (ax_temp, ax_rain) = plt.subplots(2, 1, figsize=(6, 8), sharex=True)
+ax_temp.plot(x, temperature_2m, label="Temperatura", color="red")
+ax_temp.plot(x, apparent_temperature, label="Temperatura odczuwalna")
+ax_rain.bar(x, precipitation, label="Opady")
+ax_temp.legend()
+ax_temp.set_ylabel("temperatura")
+ax_temp.set_xlabel("czas")
+ax_rain.tick_params(axis="x", rotation=45)
+
+fig.canvas.manager.set_window_title("Wykres")
+for ax in [ax_temp, ax_rain]:
+    ax.grid(True)
 plt.show()
 
-#print(data["daily"])
+# print(data["daily"])
 print(data["hourly"])
